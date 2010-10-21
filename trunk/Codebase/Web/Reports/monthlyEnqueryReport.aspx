340912d2784ca331ee19eb58f02e9b0dfadf09a7
tutul.net@gmail.com <tutul.net@gmail.com@90276fc2-287c-bd33-3ac9-73920d2c0b74>
2010-10-21 17:12:25 +0000

    Changed the HTML Construction of The Page

--- trunk/Codebase/Web/Reports/monthlyEnqueryReport.aspx
+++ trunk/Codebase/Web/Reports/monthlyEnqueryReport.aspx
@@ -5,54 +5,29 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="PageHeaderContentPlaceHolder" Runat="Server">
 </asp:Content>
+
 <asp:Content ID="Content3" ContentPlaceHolderID="SideBarPlaceHolder" Runat="Server">
 </asp:Content>
-<asp:Content ID="Content4" ContentPlaceHolderID="PageContentPlaceHolder" Runat="Server">
 
+<asp:Content ID="Content4" ContentPlaceHolderID="PageContentPlaceHolder" Runat="Server">
+    <%--Filter Box--%>
     <div class="GroupBox">
-    <table cellpadding="0" cellspacing="0" border="0"  width="100%">
-        
-        <tr>
-            <td>
-            <div class="GroupBox">
-                <table cellpadding="0" cellspacing="0" border="0" width="100%">
-                    <tr>
-                        <td style="width:10%">Select Year</td>
-                        <td style="width:10%">
-                            <asp:DropDownList ID="ddlYear" runat="server">
-                                <asp:ListItem Text="2005" Value="2005" Selected="True"></asp:ListItem>
-                                <asp:ListItem Text="2006" Value="2006"></asp:ListItem>
-                                <asp:ListItem Text="2007" Value="2007"></asp:ListItem>
-                                <asp:ListItem Text="2008" Value="2008"></asp:ListItem>
-                                <asp:ListItem Text="2009" Value="2009"></asp:ListItem>
-                                <asp:ListItem Text="2010" Value="2010"></asp:ListItem>
-                                <asp:ListItem Text="2011" Value="2011"></asp:ListItem>
-                                <asp:ListItem Text="2012" Value="2012"></asp:ListItem>
-                                <asp:ListItem Text="2013" Value="2013"></asp:ListItem>
-                                <asp:ListItem Text="2014" Value="2014"></asp:ListItem>
-                                <asp:ListItem Text="2015" Value="2015"></asp:ListItem>
-                                <asp:ListItem Text="2016" Value="2016"></asp:ListItem>
-                                
-                            </asp:DropDownList>
-                        </td>
-                        <td >
-                            <asp:Button ID="btnShowReport" runat="server" Text="Show Report" 
-                                onclick="btnShowReport_Click" />
-                        </td>
-                    </tr>
-                </table>
-                </div>
-            </td>
-        </tr>
-        
-        <tr>
-            <td>
-                <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" />
-            </td>
-        </tr>
-    </table>
+        <div class="floatleft">Select Year:</div>
+        <div class="floatleft" style="margin-left:10px; width:120px;">
+            <asp:DropDownList ID="ddlYear" runat="server" CssClass="DropDownListCommon"></asp:DropDownList>
+        </div>
+        <div class="floatleft" style="margin-left:10px;">
+            <asp:Button ID="btnShowReport" runat="server" Text="Show Report" onclick="btnShowReport_Click" />
+        </div>
+        <div class="clearboth"></div>
+    </div> 
+         
+    <%--Crystal Report Viewer Box--%>
+    <div id="divReportContainer" runat="server" visible="false" class="GroupBox" style="margin-bottom:0px;">
+        <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" />
     </div>
 </asp:Content>
 
--- trunk/Codebase/Web/Reports/monthlyEnqueryReport.aspx.cs
+++ trunk/Codebase/Web/Reports/monthlyEnqueryReport.aspx.cs
@@ -10,11 +10,21 @@ using CrystalDecisions.CrystalReports.Engine;
 using CrystalDecisions.Shared;
 
 
-public partial class Reports_monthlyEnqueryReport : System.Web.UI.Page
+public partial class Reports_monthlyEnqueryReport : BasePage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            BindYearDropDownlist();
+        }
+    }
+    protected void BindYearDropDownlist()
+    {
+        for (int i = DateTime.Now.Year - 5; i <= DateTime.Now.Year + 5; i++)
+        {
+            ddlYear.Items.Add(new ListItem(i.ToString(), i.ToString()));
+        }
     }
     protected void btnShowReport_Click(object sender, EventArgs e)
     {
@@ -25,7 +35,7 @@ public partial class Reports_monthlyEnqueryReport : System.Web.UI.Page
                      select i;
         DataTable dtEnqueryDetails = LINQToDataTable(query1);
 
-        
+
 
         ReportDocument repDoc = new ReportDocument();
         repDoc.Load(HttpContext.Current.Request.PhysicalApplicationPath.Trim()
@@ -36,53 +46,50 @@ public partial class Reports_monthlyEnqueryReport : System.Web.UI.Page
         repDoc.SetDataSource(dtEnqueryDetails);
 
         CrystalReportViewer1.ReportSource = repDoc;
-
+        divReportContainer.Visible = true;
     }
 
 
     public DataTable LINQToDataTable<T>(IEnumerable<T> varlist)
-{
-     DataTable dtReturn = new DataTable();
+    {
+        DataTable dtReturn = new DataTable();
 
-     // column names 
-     PropertyInfo[] oProps = null;
+        // column names 
+        PropertyInfo[] oProps = null;
 
-     if (varlist == null) return dtReturn;
+        if (varlist == null) return dtReturn;
 
-     foreach (T rec in varlist)
-     {
-          // Use reflection to get property names, to create table, Only first time, others 
-          //will follow 
-          if (oProps == null)
-          {
-               oProps = ((Type)rec.GetType()).GetProperties();
-               foreach (PropertyInfo pi in oProps)
-               {
+        foreach (T rec in varlist)
+        {
+            // Use reflection to get property names, to create table, Only first time, others 
+            //will follow 
+            if (oProps == null)
+            {
+                oProps = ((Type)rec.GetType()).GetProperties();
+                foreach (PropertyInfo pi in oProps)
+                {
                     Type colType = pi.PropertyType;
 
-                    if ((colType.IsGenericType) && (colType.GetGenericTypeDefinition()      
-                    ==typeof(Nullable<>)))
-                     {
-                         colType = colType.GetGenericArguments()[0];
-                     }
+                    if ((colType.IsGenericType) && (colType.GetGenericTypeDefinition()
+                    == typeof(Nullable<>)))
+                    {
+                        colType = colType.GetGenericArguments()[0];
+                    }
 
                     dtReturn.Columns.Add(new DataColumn(pi.Name, colType));
-               }
-          }
+                }
+            }
 
-          DataRow dr = dtReturn.NewRow();
+            DataRow dr = dtReturn.NewRow();
 
-          foreach (PropertyInfo pi in oProps)
-          {
-               dr[pi.Name] = pi.GetValue(rec, null) == null ?DBNull.Value :pi.GetValue
-               (rec,null);
-          }
+            foreach (PropertyInfo pi in oProps)
+            {
+                dr[pi.Name] = pi.GetValue(rec, null) == null ? DBNull.Value : pi.GetValue
+                (rec, null);
+            }
 
-          dtReturn.Rows.Add(dr);
-     }
-     return dtReturn;
-}
-
-
-
+            dtReturn.Rows.Add(dr);
+        }
+        return dtReturn;
+    }
 }
